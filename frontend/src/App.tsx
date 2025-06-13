a6196e8e7cbeb8663282c5b94f0dfc3f5c3b71f0
IacobVictoria <161374115+IacobVictoria@users.noreply.github.com>
2025-06-13 19:23:47 +0300

    Configure tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,8 +21,8 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className='text-red-500'>
+          Editt <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
